4f4d0b6a4ad27b18babbc476d45b86c679a18e28
Xyrai <Xyraidev@gmail.com>
2019-12-27 19:24:21 +0100

    Added custom dummy menu

--- app/src/main/java/com/example/lostarkhelper/ui/classes/ClassesFragment.kt
+++ app/src/main/java/com/example/lostarkhelper/ui/classes/ClassesFragment.kt
@@ -1,14 +1,18 @@
 package com.example.lostarkhelper.ui.classes
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.lostarkhelper.R
+import android.view.MenuInflater
+
+
+
+
+
 
 class ClassesFragment : Fragment() {
 
@@ -19,13 +23,31 @@ class ClassesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        classesViewModel =
-            ViewModelProviders.of(this).get(ClassesViewModel::class.java)
+        setHasOptionsMenu(true)
+        classesViewModel = ViewModelProviders.of(this).get(ClassesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_classes, container, false)
         val textView: TextView = root.findViewById(R.id.text_classes)
         classesViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.custom_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_delete -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
